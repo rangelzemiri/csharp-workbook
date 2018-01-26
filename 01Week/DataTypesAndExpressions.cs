027e3c4239aee6ead2a1753dd20b3b2b891e4ffd
Zemiri Rangel <rangelzemiri@ahoo.com>
2018-01-25 20:47:31 -0600

    add new code

--- 01Week/DataTypesAndExpressions.cs
+++ 01Week/DataTypesAndExpressions.cs
@@ -5,8 +5,27 @@ public class Program
 {
     public static void Main()
     {
-        // your code goes here
+        bool isHuman = true;
 
+        bool f = false;
+
+        decimal num = 9.99m;
+
+        decimal total = num * num;
+
+        string firstName = "Zemiri";
+        string lastName = "Rangel";
+        int age = 20;
+        string job = "Music Producer";
+        string favoriteBand = "Coheed and Cambria";
+        string favoriteSportsTeam = "Spurs";
+
+        Console.WriteLine("First Name: " + firstName);
+        Console.WriteLine("Last Name: " + lastName);
+        Console.WriteLine("Age: " + age);
+        Console.WriteLine("Job: " + job);
+        Console.WriteLine("Favorite Band: " + favoriteBand);
+        Console.WriteLine("Favorite Sports Team: " + favoriteSportsTeam);
 
         // leave this command at the end so your program does not close automatically
         Console.ReadLine();
