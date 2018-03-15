34e2fca291dbbf935a03cb990abe1a51e029de44
Zemiri Rangel <rangelzemiri@ahoo.com>
2018-03-15 18:37:16 -0500

    commit

--- 05Week/BlogPost.cs
+++ 05Week/BlogPost.cs
@@ -2,8 +2,68 @@ using System;
 
 public class Program
 {
-	public static void Main()
-	{
-		Console.WriteLine("Hello World");
-	}
+    public static string _command;
+
+    public static void Main()
+    {
+        var newPost = new BlogPost
+        {
+            Title = "New Post",
+            Description = "New Post Description."
+        };
+        Console.WriteLine("-------------------------");
+        Console.WriteLine(newPost.Title);
+        Console.WriteLine(newPost.Description);
+        Console.WriteLine(newPost.CreationDateTime);
+        Console.WriteLine("-------------------------");
+
+        do
+        {
+            //Console.WriteLine("Number of votes: " + newPost.DisplayVotes());
+            Console.WriteLine("Please Type Like or Dislike to vote on Blog");
+            _command = Console.ReadLine().ToLower();
+
+            switch (_command)
+            {
+                case "like":
+                    newPost.UpVote();
+                    Console.WriteLine("You have " + newPost.DisplayVotes() + " votes");
+                    break;
+
+                case "dislike":
+                    newPost.DownVote();
+                    Console.WriteLine("You have " + newPost.DisplayVotes() + " votes");
+                    break;
+
+                default:
+                    Console.WriteLine("Please enter a valid command");
+                    break;
+
+            }
+        } while (_command != "exit");
+
+    }
+}
+
+class BlogPost
+{
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public readonly DateTime CreationDateTime = DateTime.Now;
+    private int _votes = 0;
+
+    public int DisplayVotes()
+    {
+        return _votes;
+    }
+
+    public void UpVote()
+    {
+        _votes++;
+    }
+
+    public void DownVote()
+    {
+        _votes--;
+    }
 }
